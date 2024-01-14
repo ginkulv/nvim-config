2618a02ab1938bbbf013c5ff7aab11f25d1e5af2
ginkulv <geenkool@yandex.ru>
2024-01-14 11:16:41 +0300

    change telescope theme to ivy

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,8 +6,8 @@ require('telescope').setup {
     }
 }
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>pf', '<cmd>Telescope find_files theme=ivy<CR>', {})
+vim.keymap.set('n', '<leader>pg', '<cmd>Telescope git_files theme=ivy<CR>', {})
+vim.keymap.set('n', '<leader>ps', '<cmd>Telescope live_grep theme=ivy<CR>', {})
 vim.keymap.set('n', '<leader>ph', '<cmd>Telescope treesitter symbols=function<CR>', {})
-vim.keymap.set('n', '<leader>pg', builtin.git_status, {})
+vim.keymap.set('n', '<leader>pg', '<cmd>Telescope git_status theme=ivy<CR>', {})
